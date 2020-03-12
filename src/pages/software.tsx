7e3e08908e5f343457436cacce358aef35bf1bf2
Arthur Fücher <arthurfucher@gmail.com>
2020-03-11 22:58:57 -0300

    cria pagina de software

---
+++ src/pages/software.tsx
@@ -0,0 +1,82 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import pegasus from "../images/peg-Banner-photon.gif";
+import "./software.css"
+
+export default function SoftwarePage() {
+  return (
+  <Layout>
+    <SEO title="Software Astrológico: Pegasus" />
+    <div style={{"display" : "flex"}}>
+      <img src={pegasus} />
+      <div style={{"marginLeft":"10px"}}>
+        <h4> Novidades da versão Photon SE</h4>
+        <ul style={{"listStyle":"none", "fontSize":"12px", "marginLeft":"0"}}>
+          <li>Efemérides com a excepcional precisão de 0.001"</li>
+          <li>Calcula posições dos planetas, pontos, asteróides e uranianos.</li>
+          <li>Impressão de mandalas coloridas configuráveis pelo usuário.</li>
+          <li>Impressão dos graus dos planetas no mapa.</li>
+          <li>Arquivamento dos dados da retificação de hora.</li>
+          <li>Configuração das orbes de aspectos por planeta.</li>
+        </ul>
+      </div>
+    </div>
+    
+    <p>Conheça o programa de astrologia Pegasus em sua nova versão Photon SE 9.0</p>
+    <p>A melhor forma é utilizar a versão deste programa de Astrologia na sua forma completa por 15 dias, siga as instruções e solicite seu programa, clicando em:</p>
+    <p> >>> quero conhecer o novo Pegasus Photon SE </p>
+    <br />
+
+    <h2>Opções e características do Pegasus Photon SE</h2>
+    <h3>Efetua dezenas de cálculos astrológicos, dentre os quais destacam-se:</h3>
+    <ul>
+      <li>Mapa Radical, com informações detalhadas, incluindo quadro completo de aspectos e cálculos de Astrologia Tradicional (ou Clássica)</li>
+      <li>Calcula posições dos planetas, pontos, asteróides e uranianos</li>
+      <li>Harmônicas</li>
+      <li>Pontos Médios / Pontos Arábicos</li>
+      <li>Estrelas</li>
+      <li>Direções primárias e secundárias</li>
+      <li>Progressão Sinódica</li>
+      <li>Direções simbólicas</li>
+      <li>Fatum / Life-Time / Pró-Luna / Age Point</li>
+      <li>Retificação de Horário</li>
+      <li>Trânsitos Diários</li>
+      <li>Trânsitos de Planetas Lentos e Rápidos</li>
+      <li>Lunação / Trânsito Diurnal</li>
+      <li>Astrologia Eletiva</li>
+      <li>Mapa do Local Space</li>
+      <li>Revolução de Sol até Saturno</li>
+      <li>Progressão da Revolução Solar</li>
+      <li>Sinastria: Mapa do Parceiro/ Interaspectos / Mapa Composto / Mapa de Relacionamento</li>      
+      <li>Luas Nova/ Crescente/ Cheia/ Minguante</li>
+      <li>Grandes conjunções (Mapa e Desenvolvimento)</li>
+      <li>Ingresso de Sol e Lua em todos os signos</li>
+      <li>Tabelas de Corte de Cabelo e Depilação</li>
+      <li>Inúmeras opções de configuração</li>
+    </ul>
+
+    <h3>Características gerais do Pegasus Photon SE</h3>
+    <ul>
+      <li>Efemérides com a excepcional precisão de 0.001"</li>
+      <li>O Pegasus SE é um software que efetua uma série de cálculos astrológicos, englobando astrologia individual, mundial e empresarial.</li>
+      <li>Extrema facilidade de uso</li>
+      <li>Inúmeras opções de configuração.</li>
+      <li>Informações priorizadas a partir do uso da maioria dos astrólogos</li>
+      <li>Informações rapidamente disponíveis</li>
+      <li>Impressão de todas as informações da tela</li>
+      <li>Excelente interface gráfica</li>
+      <li>Arquivos de coordenadas geográficas com quase 100 mil cidades do Brasil e do Mundo</li>
+    </ul>
+    
+    <h3>Exigências básicas de hardware e software</h3>
+    <ul>
+      <li>PC CPU Pentium ou superior.</li>
+      <li>Mouse indispensável</li>
+      <li>Vídeo com resolução de 1280 x 768 pontos</li>
+      <li>Sistema Operacional Windows: 7/8/10</li>
+      <li>Conexão com internet (software com proteção contra cópias via internet)</li>
+      <li>Identifica a máquina instalada. </li>
+    </ul>
+  </Layout>
+)}
