f56d1d9ed1e6069e601314272f9a2cc3b3df06cb
Arthur Fücher <arthurfucher@gmail.com>
2020-04-17 23:03:11 -0300

    adiciona estilo no botão de se inscrever

--- src/components/card.js
+++ src/components/card.js
@@ -4,6 +4,7 @@ import { navigate } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Card = ({title, date, time, teacher, id}) => {
+  const href = `mailto:astrologia@regulus.com.br?subject=${title} - ${date} - ${time}&body=Desejo me inscrever na turma.`;
     return <div className={styles.card}>
         <div className={styles.main} onClick={() => navigate(`/cursos/${id}`)}>
             <h3>{title}</h3>
@@ -12,7 +13,8 @@ const Card = ({title, date, time, teacher, id}) => {
             <p><FontAwesomeIcon icon={["fa","chalkboard-teacher"]} fixedWidth={true}></FontAwesomeIcon> {teacher}</p>
         </div>
         <div className={styles.cardBottom}>
-           <a href={`mailto:astrologia@regulus.com.br?subject=${title} - ${date} - ${time}&body=Desejo me inscrever na turma.`}>Inscreva-se nessa turma</a> 
+           <a href={href}
+           className={styles.link}>Se inscrever</a> 
         </div>
     </div>
 }
